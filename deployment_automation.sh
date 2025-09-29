930329eb8af3426504ab5ac3759e761f3a10ead9
Vijayrathan <vijayrathank@gmail.com>
2025-09-29 19:37:34 -0400

    script

---
+++ deployment_automation.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+
+INSTALL_DIR="$HOME/miniconda3"
+
+echo "Downloading Miniconda installer"
+curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+
+echo "Installing Miniconda to $INSTALL_DIR..."
+bash miniconda.sh -b -p "$INSTALL_DIR"
+
+
+export PATH="$INSTALL_DIR/bin:$PATH"
+
+echo "Initializing Conda..."
+conda init bash
+
+
+
+echo "Applying changes to current shell..."
+source ~/.bashrc
+
+echo "----Miniconda is installed and ready to use in this terminal."
+
+echo "Verifying installation..."
+conda --version
+
+
+echo "Starting Git installation..."
+
+if command -v apt-get &> /dev/null; then
+    echo "Detected apt package manager. Updating package list..."
+    sudo apt-get update -y
+    echo "Installing Git..."
+    sudo apt-get install git -y
+else
+    echo "Error: Could not find a supported package manager (apt, dnf, yum)." >&2
+    exit 1
+fi
+
+if command -v git &> /dev/null; then
+    echo " Git has been successfully installed."
+    git --version
+else
+    echo " Git installation failed." >&2
+    exit 1
+fi
+
+
+# API BASED
+
+git init
+
+git clone https://github.com/Thameem022/mlops-cs-1.git
+
+cd ./mlops-cs-1
+
+pip install -r requirements.txt
+
+nohup your_command > my.log 2>&1 < /dev/null &
+
+echo "------API running------"
+
+cd ..
+
+# local 
+
+git clone https://github.com/Thameem022/case-study-1-local.git
+
+cd ./case-study-1-local
+
+pip install -r requirements.txt
+
+nohup your_command > my.log 2>&1 < /dev/null &
+
+echo "------local running------"
+
+echo "end of script"
